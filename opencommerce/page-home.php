407f49ac31689861ba4e901ecfc4390b9db84bd3
Hugo Costa <hugocosta@jazzz.com.br>
2021-06-21 21:47:29 -0300

    Secao planos

--- opencommerce/page-home.php
+++ opencommerce/page-home.php
@@ -2,6 +2,104 @@
 <?php  get_header(); ?>
 <main class="home">
 
+    <section class="chamada-nuvem">
+        <div class="container chamada-nuvem-content">
+            <div class="img-nuvem">
+                <a href="https://www.nuvemshop.com.br/" target="_blank">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/nuvem-logo.png" alt="Nuvem Shop">
+                </a>
+            </div>
+            <div class="nuvem-info">
+                <h2>Somos parceiros oficiais da Nuvem Shop</h2>
+                <p>Somos especialistas na implantação de lojas virtuais</p>
+            </div>
+            <div class="info-parceiro">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/selo-parceiro.png" alt="Parceiro Business">
+            </div>
+        </div>
+    </section>
+    <section class="chamada-servico">
+        <div class="container">
+            <div class="head-wraper">
+                <h2>Foque no seu negócio e deixe a tecnologia com a gente!</h2>
+                <h2>Temos a solução completa para sua loja virtual vender.</h2>
+                <p>Gerencie seus produtos, automatize suas vendas, envie seus pediidos e trabalhe em vários canais.</p>
+            </div>
+        </div>
+    </section>
+    <section class="planos-home">
+        <div class="container">
+            <div class="head-wraper">
+                <h2>Abaixo nossos planos de consultoria em implementação de Lojas Virtuais Profissionais.</h2>
+                <p>Defina o plano ideal, contrate a consultoria e tenha sua loja virtual pronta para vender a partir de 24h.</p>
+            </div>
+            <div class="cont-planos">
+                <article class="item-plano">
+                    <h3>Plano Top</h3>
+                    <div class="prince">
+                        <span>Apenas 10x</span>
+                        <span class="valor">R$299,00</span>
+                    </div>
+                    <div class="info-plano">
+                        <p>Contrate a consultoria e tenha sua loja pronta para vender em até 24h com todas as funcionalidades básicas</p>
+                        <a href="">Detalhe do Plano</a>
+                    </div>
+                    <div class="btn-plano">
+                        <a href="">Solicitar Plano</a>
+                    </div>
+                </article>
+                <article class="item-plano">
+                    <h3>Plano Top</h3>
+                    <div class="prince">
+                        <span>Apenas 10x</span>
+                        <span class="valor">R$299,00</span>
+                    </div>
+                    <div class="info-plano">
+                        <p>Contrate a consultoria e tenha sua loja pronta para vender em até 24h com todas as funcionalidades básicas</p>
+                        <a href="">Detalhe do Plano</a>
+                    </div>
+                    <div class="btn-plano">
+                        <a href="">Solicitar Plano</a>
+                    </div>
+                </article>
+                <article class="item-plano">
+                    <h3>Plano Top</h3>
+                    <div class="prince">
+                        <span>Apenas 10x</span>
+                        <span class="valor">R$299,00</span>
+                    </div>
+                    <div class="info-plano">
+                        <p>Contrate a consultoria e tenha sua loja pronta para vender em até 24h com todas as funcionalidades básicas</p>
+                        <a href="">Detalhe do Plano</a>
+                    </div>
+                    <div class="btn-plano">
+                        <a href="">Solicitar Plano</a>
+                    </div>
+                </article>
+                <article class="item-plano">
+                    <h3>Plano Top</h3>
+                    <div class="prince">
+                        <span>Apenas 10x</span>
+                        <span class="valor">R$299,00</span>
+                    </div>
+                    <div class="info-plano">
+                        <p>Contrate a consultoria e tenha sua loja pronta para vender em até 24h com todas as funcionalidades básicas</p>
+                        <a href="">Detalhe do Plano</a>
+                    </div>
+                    <div class="btn-plano">
+                        <a href="">Solicitar Plano</a>
+                    </div>
+                </article>
+            </div>
+            <div class="chamada-duvidas">
+                <h4>Se tiver dúvidas, não tem problema.</h4>
+                <p>Fale com nosso consultor e ele irá te orientar sobre qual pano é mais funcional para o seu negócio.</p>
+                <div class="btn-solicitar">
+                    <a href="">Solicitar contato comercial</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php get_footer()?>
